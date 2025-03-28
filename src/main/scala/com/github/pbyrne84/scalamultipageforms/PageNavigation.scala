f6ca8b11fd38d7044e148f2d8429163ad73f83d7
Patrick Byrne <pbyrne84@gmail.com>
2025-03-28 13:43:45 +0000

    x

--- src/main/scala/com/github/pbyrne84/scalamultipageforms/PageNavigation.scala
+++ src/main/scala/com/github/pbyrne84/scalamultipageforms/PageNavigation.scala
@@ -11,7 +11,6 @@ class PageNavigation {
       case StartPage :: Nil =>
         lastAnswer match {
           case value: IntValue => calculateNextPageFromStartPage(value.value)
-
           case _ =>
             Left(s"Unsupported answer type ${lastAnswer.getClass.getSimpleName}, expected IntValue")
         }
@@ -26,13 +25,9 @@ class PageNavigation {
       3 -> ThirdPageA
     )
 
-    val maybeA = valueToNextPageMap
+    valueToNextPageMap
       .get(int)
-
-    val option: Option[Either[String, Page]] = maybeA
-      .map(Right.apply)
-
-    option
+      .map(Right.apply[String, Page])
       .getOrElse(Left(s"Invalid option $int, ${valueToNextPageMap.keys.mkString(",")}"))
   }
 }
